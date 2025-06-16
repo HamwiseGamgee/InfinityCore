883efa76247f8c7796dc2e62b3f8eb6454b4eec1
HamwiseGamgee <immkyoung@gmail.com>
2025-06-16 14:22:27 -0400

    Tried to Add Damage Bonus

--- module/apps/item.js
+++ module/apps/item.js
@@ -4,45 +4,69 @@ export default class InfinityCoreItem extends Item {
     /**
      * Optional: Add item-specific roll logic
      */
-    async roll() {
-        const flat = this.system.flatdamage || 0;
-        const dice = this.system.damagedice || 0;
+async roll() {
+    const flat = this.system.flatdamage || 0;
+    const baseDice = this.system.damagedice || 0;
 
-        const roll = await new Roll(`${dice}d6`).roll({ async: true });
+    // Get actor and attack attribute
+    const actor = this.actor;
+    const attackWith = this.system.attackwith;
 
-        const results = roll.terms[0].results;
-        for (let r of results) {
-            r.classes = [];
-            if (r.result <= 2) r.classes.push("success-die");
-            else if (r.result === 6) r.classes.push("effect-die");
-        }
-
-        const successes = getSuccessesFromBattleRoll(roll);
-        const effects = getEffectsFromBattleRoll(roll);
-        const totalDamage = flat + successes;
-        const diceHTML = results.map(r => {
-            let cls = "d6-result";
-            if (r.result <= 2) cls += " success-die";
-            else if (r.result === 6) cls += " effect-die";
-            return `<span class="${cls}">${r.result}</span>`;
-        }).join(" ");
-
-        ChatMessage.create({
-            speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-            rolls: [roll],
-            content: `
-    <div class="dice-roll">
-      <div class="dice-result">
-        <strong>${this.name} Damage Roll</strong><br>
-        <em>Dice Rolled:</em> ${diceHTML}<br>
-        <em>Flat Damage:</em> ${flat}<br>
-        <em>Successes (1-2):</em> ${successes}<br>
-        <em>Effects (6s):</em> ${effects}<br>
-        <strong>Total Damage:</strong> ${totalDamage}
-      </div>
-    </div>
-    `
-        });
-
+    // Safely get the attribute value from the actor
+    let attributeValue = 0;
+    if (actor && attackWith && actor.system.attributes?.[attackWith]?.value) {
+        attributeValue = actor.system.attributes[attackWith].value;
     }
+
+    // Bonus damage dice based on attribute value
+    function getBonusDamageDice(attributeValue) {
+        if (attributeValue <= 8) return 0;
+        return Math.floor((attributeValue - 7) / 2);
+    }
+
+    const bonusDice = getBonusDamageDice(attributeValue);
+    const totalDice = baseDice + bonusDice;
+
+    // Roll damage
+    const roll = await new Roll(`${totalDice}d6`).roll({ async: true });
+
+    // Parse roll results
+    const results = roll.terms[0].results;
+    for (let r of results) {
+        r.classes = [];
+        if (r.result <= 2) r.classes.push("success-die");
+        else if (r.result === 6) r.classes.push("effect-die");
+    }
+
+    const successes = getSuccessesFromBattleRoll(roll);
+    const effects = getEffectsFromBattleRoll(roll);
+    const totalDamage = flat + successes;
+
+    // Display results
+    const diceHTML = results.map(r => {
+        let cls = "d6-result";
+        if (r.result <= 2) cls += " success-die";
+        else if (r.result === 6) cls += " effect-die";
+        return `<span class="${cls}">${r.result}</span>`;
+    }).join(" ");
+
+    ChatMessage.create({
+        speaker: ChatMessage.getSpeaker({ actor }),
+        rolls: [roll],
+        content: `
+<div class="dice-roll">
+  <div class="dice-result">
+    <strong>${this.name} Damage Roll</strong><br>
+    <em>Base Dice:</em> ${baseDice} + <em>Bonus Dice:</em> ${bonusDice} (from ${attackWith} ${attributeValue})<br>
+    <em>Dice Rolled:</em> ${diceHTML}<br>
+    <em>Flat Damage:</em> ${flat}<br>
+    <em>Successes (1-2):</em> ${successes}<br>
+    <em>Effects (6s):</em> ${effects}<br>
+    <strong>Total Damage:</strong> ${totalDamage}
+  </div>
+</div>
+`
+    });
+}
+
 }
